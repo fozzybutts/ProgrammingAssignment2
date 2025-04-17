b819d81155bc4e2485b64d18528a1a9ff0defcec
Evan Gentile <ewgentile@gmail.com>
2025-04-17 17:53:20 -0400

    player movement is done

--- Assets/scripts/PLAYER MOVE.cs
+++ Assets/scripts/PLAYER MOVE.cs
@@ -6,10 +6,28 @@ public class PLAYERMOVE : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        Vector2 direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
         {
-            //transform.position.x += 1;
+            direction = Vector2.up;
         }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            direction = Vector2.down;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            direction = Vector2.left;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            direction = Vector2.right;
+        }
+
+        float dt = Time.deltaTime;
+        float speed = 10.0f;
+        Vector3 change = direction * speed * dt;
+        transform.position = transform.position + change;
 
 
     }
