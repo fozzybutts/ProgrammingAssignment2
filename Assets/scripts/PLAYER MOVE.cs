e651413b26ab5ad61282b7f085d895ff06a1b182
Evan Gentile <ewgentile@gmail.com>
2025-04-17 16:41:28 -0400

    added movement script (LET ME DO IT)

---
+++ Assets/scripts/PLAYER MOVE.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PLAYERMOVE : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //transform.position.x += 1;
+        }
+
+
+    }
+}
